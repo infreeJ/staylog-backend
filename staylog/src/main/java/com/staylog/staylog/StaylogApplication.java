32f79e172538920f24222c379481db164d6b59c1
Danpung <kkokko514@naver.com>
2025-10-23 12:49:46 +0900

    backend maven project first commit

---
+++ staylog/src/main/java/com/staylog/staylog/StaylogApplication.java
@@ -0,0 +1,13 @@
+package com.staylog.staylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaylogApplication.class, args);
+	}
+
+}
