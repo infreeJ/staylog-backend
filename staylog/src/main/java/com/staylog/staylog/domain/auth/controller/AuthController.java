9708c4e138f1207a94992ac9ab3eead09dee820d
infreeJ <kasious97@naver.com>
2025-10-25 17:30:11 +0900

    swagger 설명 추가

--- staylog/src/main/java/com/staylog/staylog/domain/auth/controller/AuthController.java
+++ staylog/src/main/java/com/staylog/staylog/domain/auth/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.staylog.staylog.domain.auth.dto.response.LoginResponse;
 import com.staylog.staylog.domain.auth.dto.response.TokenResponse;
 import com.staylog.staylog.domain.auth.service.AuthService;
 import com.staylog.staylog.global.common.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Tag(name = "AuthController", description = "인증/인가 API")
 @RestController
 @RequestMapping("/api")
 @Slf4j
@@ -27,6 +30,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인", description = "사용자 로그인")
     @PostMapping("/auth/login")
     public ResponseEntity<SuccessResponse<LoginResponse>> login(@RequestBody @Valid LoginRequest loginRequest,
                                                                HttpServletRequest request,
@@ -40,6 +44,7 @@ public class AuthController {
 
     }
 
+    @Operation(summary = "로그아웃", description = "사용자 로그아웃")
     @PostMapping("/logout")
     public ResponseEntity<SuccessResponse<Void>> logout(
             HttpServletRequest request,
@@ -49,6 +54,7 @@ public class AuthController {
         return ResponseEntity.ok(SuccessResponse.of("로그아웃 성공", null));
     }
 
+    @Operation(summary = "토큰 재발급", description = "RefreshToken을 이용해서 만료된 AccessToken을 재발급합니다.")
     @PostMapping("/refresh")
     public ResponseEntity<SuccessResponse<TokenResponse>> refresh(
             HttpServletRequest request,
@@ -59,6 +65,8 @@ public class AuthController {
 
     }
 
+
+    @Operation(summary = "사용자 회원가입", description = "인증이 완료된 이메일을 이용해서 회원가입합니다. \n * userId는 제외하고 데이터를 기입해야합니다.")
     @PostMapping("/user")
     public ResponseEntity<SuccessResponse<Map<String, Object>>> signup(@RequestBody SignupRequest signupRequest) {
         long userId = authService.signupUser(signupRequest);
--- staylog/src/main/java/com/staylog/staylog/domain/auth/controller/MailController.java
+++ staylog/src/main/java/com/staylog/staylog/domain/auth/controller/MailController.java
@@ -3,6 +3,8 @@ package com.staylog.staylog.domain.auth.controller;
 import com.staylog.staylog.domain.auth.dto.request.MailCheckRequest;
 import com.staylog.staylog.domain.auth.dto.request.MailSendRequest;
 import com.staylog.staylog.domain.auth.service.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Tag(name = "MailController", description = "이메일 인증 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -21,13 +24,15 @@ public class MailController {
 
     private final MailService mailService;
 
-
+    @Operation(summary = "인증 메일 전송", description = "인증 메일을 사용자에게 전송합니다. \n * 인증 코드는 10분 동안 유효합니다.")
     @PostMapping("/mail-send")
     public ResponseEntity<Map<String, String>> sendVerificationMail(@RequestBody MailSendRequest requestDto) {
         mailService.sendVerificationMail(requestDto.getEmail());
         return ResponseEntity.ok(Map.of("message", "인증 메일이 성공적으로 발송되었습니다."));
     }
-
+    
+    
+    @Operation(summary = "인증 코드 검증", description = "사용자가 입력한 인증 코드를 검증합니다.")
     @PostMapping("/mail-check")
     public ResponseEntity<Map<String, String>> checkVerificationMail(@RequestBody MailCheckRequest requestDto) {
         boolean isVerified = mailService.verifyMail(requestDto.getEmail(), requestDto.getCode());
