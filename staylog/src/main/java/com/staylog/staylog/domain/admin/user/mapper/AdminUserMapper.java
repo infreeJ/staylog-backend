84e02c4480c09a95f9bcbc3b59796c5eeed5f157
youngseok <gys329@naver.com>
2025-10-27 09:25:20 +0900

    feat: 관리자 유저 관련 Mapper,dto 추가

--- staylog/src/main/java/com/staylog/staylog/domain/admin/user/mapper/AdminUserMapper.java
+++ staylog/src/main/java/com/staylog/staylog/domain/admin/user/mapper/AdminUserMapper.java
@@ -1,5 +1,6 @@
 package com.staylog.staylog.domain.admin.user.mapper;
 
+import com.staylog.staylog.domain.admin.user.dto.request.AdminGetUserDetailRequest;
 import com.staylog.staylog.domain.user.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,7 +9,11 @@ import java.util.List;
 @Mapper
 public interface AdminUserMapper {
     /**
-     * 유저 목록 조회 매퍼
+     * 유저 목록 조회
+     *
+     * @param req 검색 조건, 페이지 정보 담은 요청 객체
+     * @Date 2025.10.27
+     * @Return 유저 목록
      */
-    List<UserDto> findUsers();
+    List<UserDto> findUsers(AdminGetUserDetailRequest.AdminUserSearchReq req);
 }
