6d765c1fc2daa6fb00076b73dd64b7d48e153efa
Danpung <kkokko514@naver.com>
2025-11-05 09:57:17 +0900

    [수정] 파일명 변경 관련 로직 수정

--- staylog/src/main/java/com/staylog/staylog/global/common/util/FileUtil.java
+++ staylog/src/main/java/com/staylog/staylog/global/common/util/FileUtil.java
@@ -5,8 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -20,12 +18,16 @@ public class FileUtil {
         }
 
         String originalName = file.getOriginalFilename();
-        // 파일명 인코딩
-        String encodedName = URLEncoder.encode(originalName, StandardCharsets.UTF_8.toString());
-        String uuid = UUID.randomUUID().toString();
+        // 파일 확장자 추출
+        String extension = "";
+		if (originalName != null && originalName.contains(".")) {
+			extension = originalName.substring(originalName.lastIndexOf("."));
+		}
+        // UUID와 확장자를 조합하여 완전히 새로운 파일명 생성
+        String safeFileName = UUID.randomUUID().toString() + extension;
         String datePath = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
         // 시스템 기본 경로 구분자 대신 URL에 적합한 '/'를 사용하도록 변경
-        String savedPath = Paths.get(datePath, uuid + "_" + encodedName).toString().replace("\\\\", "/");
+        String savedPath = Paths.get(datePath, safeFileName).toString().replace("\\\\", "/");
         File destinationFile = new File(uploadPath, savedPath);
 
         destinationFile.getParentFile().mkdirs();
