f974caaba4f5a8932385a344a76976ab599f0437
minaoh <ohmina0123@naver.com>
2025-11-08 10:45:21 +0900

    [수정] 예약정보 & 리뷰내역 수정

--- staylog/src/main/java/com/staylog/staylog/domain/mypage/dto/response/ReviewInfoResponse.java
+++ staylog/src/main/java/com/staylog/staylog/domain/mypage/dto/response/ReviewInfoResponse.java
@@ -9,12 +9,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class ReviewInfoResponse {
-    private Long reviewId; //리뷰 고유 ID
-    private Long bookingId; //예약번호
-    private String accomodationName; //숙소명
-    private String content; //리뷰 내용 
-    private Integer rating; //별점
-    private LocalDateTime createdAt; //작성일
+    private Long reviewId; 				// 리뷰 고유 ID
+    private Long bookingId; 			// 예약 고유번호 내부 (RESERVATION.BOOKING_ID(PK))
+    private String bookingNum; 			// 회원에게 보이는 예약번호 (RESERVATION.BOOKING_NUM)
+    private String accommodationName; 	// 숙소명
+    private String checkIn; 			// 체크인
+    private String checkOut; 		  	// 체크아웃
+    private String title; 				// 리뷰 제목
+    private Integer rating; 			// 별점
+    private LocalDateTime createdAt; 	// 작성일
 }
 
 
