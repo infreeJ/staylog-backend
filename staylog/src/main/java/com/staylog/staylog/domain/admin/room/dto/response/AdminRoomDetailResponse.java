db42c426b2eed8ea5fc36a0612fa2bbfa8df8272
chunseunghyun <cshchun1018@gmail.com>
2025-11-10 00:37:09 +0900

    [수정] 객실 체크인, 체크아웃 관련 삭제

--- staylog/src/main/java/com/staylog/staylog/domain/admin/room/dto/response/AdminRoomDetailResponse.java
+++ staylog/src/main/java/com/staylog/staylog/domain/admin/room/dto/response/AdminRoomDetailResponse.java
@@ -49,11 +49,6 @@ public class AdminRoomDetailResponse {
     /** 최대 유아 수 */
     private Integer maxInfant;
 
-    /** 체크인 시간 */
-    private String checkIn;
-    /** 체크아웃 시간 */
-    private String checkOut;
-
     /** 객실 면적 (m²) */
     private Double area;
 
